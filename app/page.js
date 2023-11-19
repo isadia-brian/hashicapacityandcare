5d943584c85ebb7175e57a193aeac167a658aab6
silentpalms <silentpalmsvilla@gmail.com>
2023-11-19 20:48:14 +0300

    added responsive about page

--- app/page.js
+++ app/page.js
@@ -3,17 +3,26 @@ import Image from "next/image";
 import Link from "next/link";
 import Navbar from "./components/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAnglesDown,
+  faAngleRight,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+import localFont from "next/font/local";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "700", "800", "900"],
 });
 
+const pnb_font = localFont({
+  src: "../public/assets/fonts/fancy/pnb.ttf",
+});
+
 export default function Home() {
   return (
-    <main className={`${poppins.className} relative m-4 md:m-6 `}>
-      <div className='h-screen relative'>
+    <main className={`${poppins.className} relative  `}>
+      <div className='relative  m-4 md:m-6'>
         <Navbar />
         <div className=' mt-9 py-5'>
           <div className='font-black text-left w-full relative flex items-center justify-start'>
@@ -35,17 +44,23 @@ export default function Home() {
           </div>
         </div>
 
-        <div className='h-[342px] md:h-[360px] relative rounded-[30px] '>
+        <div className='h-[342px] md:h-[420px] relative rounded-[30px] '>
           <Image
             fill
             src='/hero-large.png'
             className='object-cover rounded-[30px] hidden md:flex'
+            priority
           />
           <div className='w-[269px] h-[296px] left-0 top-[46px] absolute bg-white border border-secondary/80 md:hidden' />
           <div className='w-[212px] h-[237px]  top-0 right-0 absolute bg-primary md:hidden' />
           <div className='w-[237px] h-[272px] left-[16px] top-[58px] absolute md:hidden'>
             <div className='relative h-full w-full'>
-              <Image src='/hero.png' fill objectFit='cover' />
+              <Image
+                src='/hero.png'
+                fill
+                className='object-fit object-cover'
+                priority
+              />
             </div>
           </div>
         </div>
@@ -55,6 +70,82 @@ export default function Home() {
             beatFade
             style={{ color: "#00CCCC", fontSize: "22px" }}
           />
+        </div>
+      </div>
+
+      <div className='relative flex overflow-x-hidden md:-mt-10 '>
+        <div className='flex animate-marquee whitespace-nowrap'>
+          <span
+            className={`${pnb_font.className} text-[5rem] md:text-[12.5rem] text-fill-transparent text-stroke-2 text-stroke-secondary tracking-wider mx-8 `}>
+            HASHI CAPACITY & CARE
+          </span>
+        </div>
+        <div className='absolute top-0 animate-marquee2 whitespace-nowrap'>
+          <span
+            className={`${pnb_font.className} text-[5rem] md:text-[12.5rem] text-fill-transparent text-stroke-2 text-stroke-secondary tracking-wider mx-8 `}>
+            HASHI CAPACITY & CARE
+          </span>
+        </div>
+      </div>
+
+      <div className='relative my-3 bg-primary h-full md:h-[500px] px-4 md:px-0 grid grid-cols-1 md:grid-cols-2'>
+        <div>
+          <h4 className='flex items-center gap-3 text-lg  md:px-6 py-4'>
+            <span className='text-white font-light'>About Us</span>
+            <span>
+              <FontAwesomeIcon
+                icon={faAngleRight}
+                style={{ color: "#00CCCC", fontSize: "16px" }}
+              />
+            </span>
+            <span className='text-white font-bold'>Who We Are</span>
+          </h4>
+          <div className='relative w-full'>
+            <div className='relative h-[180px] mb-10 md:hidden '>
+              <Image
+                src='/aboutImage.png'
+                alt='about hashi image'
+                fill
+                className='rounded-xl object-cover'
+                loading='lazy'
+              />
+            </div>
+            <div className='md:absolute md:left-6 md:top-[6.25rem] md:w-[500px]'>
+              <h2 className='text-white font-bold text-lg'>
+                Hashi Capacity and Care is a premier healthcare company based in
+                Kenya.
+              </h2>
+              <p className='text-white/75 text-[0.9375rem] my-5'>
+                {" "}
+                We are dedicated to providing exceptional medical services,
+                focusing on improving the well-being of individuals and
+                communities across the country. With a team of highly skilled
+                professionals and state-of-the-art facilities, we strive to
+                deliver high-quality healthcare services that meet the diverse
+                needs of our patients.
+              </p>
+
+              <Link
+                href='#'
+                className='flex items-center gap-2 text-white/75 font-medium pb-4 md:pb-0 '>
+                <span className='underline underline-offset-4'>LEARN MORE</span>{" "}
+                <span>
+                  <FontAwesomeIcon icon={faArrowRight} fade />
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className='relative h-full  '>
+            <Image
+              src='/aboutImage.png'
+              alt='about hashi image'
+              fill
+              className='object-cover aspect-square'
+              loading='lazy'
+            />
+          </div>
         </div>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,45 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    textFillColor: (theme) => theme("borderColor"),
+    textStrokeColor: (theme) => theme("borderColor"),
+    textStrokeWidth: (theme) => theme("borderWidth"),
+    paintOrder: {
+      fsm: { paintOrder: "fill stroke markers" },
+      fms: { paintOrder: "fill markers stroke" },
+      sfm: { paintOrder: "stroke fill markers" },
+      smf: { paintOrder: "stroke markers fill" },
+      mfs: { paintOrder: "markers fill stroke" },
+      msf: { paintOrder: "markers stroke fill" },
+    },
     extend: {
       colors: {
         primary: "#008080",
         secondary: "#666666",
         accent: "#00CCCC",
       },
+      animation: {
+        marquee: "marquee 20s linear infinite",
+        marquee2: "marquee2 20s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
     },
   },
-  plugins: [],
+  variants: {
+    // all the following default to ['responsive']
+
+    textStrokeColor: ["responsive"],
+    textStrokeWidth: ["responsive"],
+    paintOrder: ["responsive"],
+  },
+  plugins: [require("tailwindcss-text-fill-stroke")()],
 };
